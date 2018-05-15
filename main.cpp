05306dc72e8cfa5715b30c98c19e5c04771bf092
TongTuyen <tongvantuyennd@gmail.com>
2018-05-15 17:08:55 +0700

    Update function set,get port

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "mainwindow.h"
 #include <QApplication>
 
@@ -11,29 +9,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}
->>>>>>> 15491401a593fcf951f6b05586a0e95e9b6d9d1a
-=======
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}
->>>>>>> 15491401a593fcf951f6b05586a0e95e9b6d9d1a
